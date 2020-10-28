244f807911efdf01d77f58940dd14eea8bd76829
Nicholas <nickknisel@gmail.com>
2020-10-28 12:14:07 -0500

    first entry

---
+++ loopsPracitice2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loopsPracitice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
